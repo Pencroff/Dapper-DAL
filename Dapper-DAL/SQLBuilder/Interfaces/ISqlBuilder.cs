02192a61b6d58ab20caa62c6e68db4e65c322da9
Sergei Danilov <pencroff@gmail.com>
2013-06-11 12:29:00 +0300

    Start creating SqlBuilder for elegant queries (like Dapper Sql Builder)
    Dapper Sql Builder includs for example

---
+++ Dapper-DAL/SQLBuilder/Interfaces/ISqlBuilder.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Dapper_DAL.SQLBuilder.Interfaces
+{
+    public interface ISqlBuilder
+    {
+        ISqlBuilder SELECT(string columns = null);
+        ISqlBuilder Column(string columnName, string columnAliace = null);
+        ISqlBuilder FROM(string tables = null);
+        ISqlBuilder Table(string tableName, string tableAliace = null);
+        ISqlBuilder WHERE(string whereConditions);
+        ISqlBuilder AndWhere(string fieldName, string condition, string parameterAliace = null);
+        ISqlBuilder OrWhere(string fieldName, string condition, string parameterAliace = null);
+    }
+}
---
+++ Dapper-DAL/SQLBuilder/SqlBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Dapper_DAL.SQLBuilder.Interfaces;
+
+namespace Dapper_DAL.SQLBuilder
+{
+    public class SqlBuilder : ISqlBuilder
+    {
+        private static ISqlBuilder _sqlBuilder;
+        public static ISqlBuilder Current 
+        {
+            get { return _sqlBuilder ?? (_sqlBuilder = new SqlBuilder()); }
+        }
+
+        public ISqlBuilder SELECT(string columns = null)
+        {
+            return this;
+        }
+
+        public ISqlBuilder Column(string columnName, string columnAliace = null)
+        {
+            return this;
+        }
+
+        public ISqlBuilder FROM(string tables = null)
+        {
+            return this;
+        }
+
+        public ISqlBuilder Table(string tableName, string tableAliace = null)
+        {
+            return this;
+        }
+
+        public ISqlBuilder WHERE(string whereConditions)
+        {
+            return this;
+        }
+
+        public ISqlBuilder AndWhere(string fieldName, string condition, string parameterAliace = null)
+        {
+            return this;
+        }
+
+        public ISqlBuilder OrWhere(string fieldName, string condition, string parameterAliace = null)
+        {
+            return this;
+        }
+    }
+}
